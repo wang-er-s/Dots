b4273f357dbf3394e4793c7a58f0eba3e5c84791
zhangguoju <zhangguoju@123.com>
2025-09-19 15:32:09 +0800

    init

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace TMG.Survivors
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private Button _playButton;
+        [SerializeField] private Button _quitButton;
+
+        private void OnEnable()
+        {
+            _playButton.onClick.AddListener(OnPlayButton);
+            _quitButton.onClick.AddListener(OnQuitButton);
+        }
+
+        private void OnDisable()
+        {
+            _playButton.onClick.RemoveAllListeners();
+            _quitButton.onClick.RemoveAllListeners();
+        }
+
+        private void OnPlayButton()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        private void OnQuitButton()
+        {
+            Application.Quit();
+        }
+    }
+}
